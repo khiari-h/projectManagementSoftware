a5bcf76a79df2783a7dbab57ab1a9a2944632353
Hamdane Khiari <76649362+khiari-h@users.noreply.github.com>
2023-03-25 07:56:45 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,18 @@
 const express = require('express');
 const app = express();
 
+const inventory = [
+  { name: 'T-shirt', quantity: 10, price: 15 },
+  { name: 'Jeans', quantity: 5, price: 50 },
+  { name: 'Sneakers', quantity: 7, price: 80 }
+];
+
 app.get('/', (req, res) => {
   res.send('Bonjour');
+});
+
+app.get('/inventory', (req, res) => {
+  res.json(inventory);
 });
 
 const port = 3000;
